b64c1d05c0fe640d39f895e9fde68c6c442978ef
Halil YILDIRIM <halilyildirim3443@gmail.com>
2026-06-06 14:16:05 +0300

    chore: register chat services in DI container

--- VoiceAiChatApi/Program.cs
+++ VoiceAiChatApi/Program.cs
@@ -1,24 +1,23 @@
+using VoiceAiChatApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<ChatOptions>(builder.Configuration.GetSection(ChatOptions.SectionName));
+builder.Services.AddHttpClient<OllamaResponder>();
+builder.Services.AddSingleton<StaticResponder>();
+builder.Services.AddScoped<IChatService, ChatService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
